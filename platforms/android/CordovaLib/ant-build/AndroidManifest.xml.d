2f584b1492577b16f603ea070fc93531f0afc8c7
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-10-21 13:31:07 -0200

    Adding the code back

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/classes.dex \
+/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/CordovaLib/ant-build/classes.jar \
