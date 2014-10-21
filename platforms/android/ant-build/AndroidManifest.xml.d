07b1e72568f4eec91423837cbf231aa9ae4d2a4c
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-10-21 13:29:43 -0200

    Remove the now ignored directories and files

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/AndroidManifest.xml \
--- platforms/android/ant-build/classes.dex.d
+++
@@ -1,9 +0,0 @@
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/classes.dex : \
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/classes/com/phonegap/helloworld/BuildConfig.class \
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/classes/com/phonegap/helloworld/CordovaApp.class \
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/classes/com/phonegap/helloworld/R$attr.class \
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/classes/com/phonegap/helloworld/R$drawable.class \
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/classes/com/phonegap/helloworld/R$string.class \
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/classes/com/phonegap/helloworld/R$xml.class \
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/classes/com/phonegap/helloworld/R.class \
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/dexedLibs/classes-6ab78a34ff819e2d7653c708411990fe.jar \
