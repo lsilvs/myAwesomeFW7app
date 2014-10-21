7d180145e877ef6c873eb95e4c12b67fbf776e68
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-10-21 13:41:25 -0200

    Removed all files that are in the .gitignore

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/CordovaApp.ap_ \
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/classes.dex \
-/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/CordovaLib/ant-build/classes.jar \
