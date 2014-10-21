cc90a7c1b8878be0536304e3f1472aaf13910401
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-10-21 13:11:58 -0200

    cordova build

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/lsilvs/Dropbox/Projects/pgFW7/myAwesomeFW7app/platforms/android/AndroidManifest.xml \
